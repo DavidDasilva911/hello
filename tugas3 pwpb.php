16e5628f49dbb8362f76f605aa5968dcc7954a34
DavidDasilva911 <55771963+DavidDasilva911@users.noreply.github.com>
2020-06-15 10:39:05 +0700

    Tugas code Pwpb3
    XI RPL1

--- tugas3 pwpb.php
+++ tugas3 pwpb.php
@@ -1,8 +1,8 @@
 <?php
-    echo'nama : Mochammad David Dasilva Akbar M.S <br>';
-    echo'kelas : XI RPL 1 <br>';
-    echo'SMK Taruna bhakti <br>';
-    echo'Jl.kampung bojong rt4 rw20 <br>';
-    echo'email : davidchibulan@gmail.com <br>';
-    echo'No Telpon : 085695948661 <br>';
+    echo 'nama : Mochammad David Dasilva Akbar M.S <br>';
+    echo 'kelas : XI RPL 1 <br>';
+    echo "SMK Taruna bhakti <br>";
+    echo "Jl.kampung bojong rt4 rw20 <br>";
+    echo 'email : davidchibulan@gmail.com <br>';
+    echo 'No Telpon : 085695948661 <br>';
 ?>
