3d9115cbf24891f506ee48dc573b7cb18b5c1a1c
DavidDasilva911 <55771963+DavidDasilva911@users.noreply.github.com>
2020-06-15 10:50:14 +0700

    Add files via upload
    Tugas pwpb 3

--- tugas3 pwpb.php
+++ tugas3 pwpb.php
@@ -1,8 +1,8 @@
 <?php
-    echo 'nama : Mochammad David Dasilva Akbar M.S <br>';
-    echo 'kelas : XI RPL 1 <br>';
-    echo "SMK Taruna bhakti <br>";
-    echo "Jl.kampung bojong rt4 rw20 <br>";
-    echo 'email : davidchibulan@gmail.com <br>';
-    echo 'No Telpon : 085695948661 <br>';
+    echo' Nama : Mochammad David Dasilva Akbar M.S <br>';
+    echo' Kelas : XI RPL 1 <br>';
+    echo "SMK Taruna bhakti";
+    echo "Jl.kampung bojong rt4 rw20";
+    echo' email : davidchibulan@gmail.com <br>';
+    echo' No Telpon : 085695948661 <br>';
 ?>
